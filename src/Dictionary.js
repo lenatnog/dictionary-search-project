552d5442f16a4f4d425ef15d91e738dff75ac103
lenatnog <helenatnog@gmail.com>
2025-11-11 17:01:41 -0300

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -7,7 +7,7 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+    alert(`Searching for the definition of ${keyword} `);
   }
 
   function handleKeywordChange(event) {
